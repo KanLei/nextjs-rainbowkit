af01a0b6c6fb0838e8e8c58ac897016be9b9e747
kanlei <kanlei@live.com>
2024-12-25 15:53:18 +0800

    feat: upgrade package

--- app/config.ts
+++ app/config.ts
@@ -10,27 +10,70 @@ import {
   base,
   zora,
   sepolia,
+  bsc,
 } from 'wagmi/chains';
 import { createClient, fallback, http } from 'viem';
 import { injected } from 'wagmi/connectors';
+import { 
+  metaMaskWallet,
+  binanceWallet,
+  okxWallet,
+  tokenPocketWallet,
+  coinbaseWallet,
+  walletConnectWallet,
+  imTokenWallet,
+  trustWallet,
+  bitgetWallet,
+  rainbowWallet, 
+} from '@rainbow-me/rainbowkit/wallets';
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 
-
-export const config = createConfig({
-  chains: [mainnet, sepolia],
-  ssr: true, // https://wagmi.sh/react/guides/ssr
-  storage: createStorage({  
-    storage: cookieStorage, 
-  }),
-  connectors: [injected()],  // https://wagmi.sh/react/api/transports/unstable_connector
+export const config = getDefaultConfig({
+  appName: 'Demo',
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ?? '',
+  chains: [mainnet, arbitrum, base, bsc, optimism, polygon],
+  wallets: [
+    {
+      groupName: 'Popular',
+      wallets: [
+        metaMaskWallet,
+        binanceWallet,
+        okxWallet,
+        tokenPocketWallet,
+        coinbaseWallet,
+        walletConnectWallet,
+        imTokenWallet,
+        trustWallet,
+        bitgetWallet,
+        rainbowWallet,
+      ]
+    }
+  ],
   transports: {
     [mainnet.id]: fallback([
       unstable_connector(injected),
-      // http('https://eth-mainnet.g.alchemy.com/v2/...'),
       http()
     ]),
     [sepolia.id]: http(),
   },
-  // client({ chain }) {
-  //   return createClient({ chain, transport: http() })
-  // }
 })
+
+// export const config = createConfig({
+//   chains: [mainnet, sepolia],
+//   ssr: true, // https://wagmi.sh/react/guides/ssr
+//   storage: createStorage({  
+//     storage: cookieStorage, 
+//   }),
+//   connectors: [injected()],  // https://wagmi.sh/react/api/transports/unstable_connector
+//   transports: {
+//     [mainnet.id]: fallback([
+//       unstable_connector(injected),
+//       // http('https://eth-mainnet.g.alchemy.com/v2/...'),
+//       http()
+//     ]),
+//     [sepolia.id]: http(),
+//   },
+//   // client({ chain }) {
+//   //   return createClient({ chain, transport: http() })
+//   // }
+// })
